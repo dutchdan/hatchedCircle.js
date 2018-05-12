00bec802e414157d0c80f5278396f7f099adc3fd
Daniel <36626223+dutchdan@users.noreply.github.com>
2018-05-12 10:16:19 +0100

    Update hatchedCircle.js
    updated the comments in the function

--- hatchedCircle.js
+++ hatchedCircle.js
@@ -6,7 +6,7 @@ hatchedCircle = function (x, y, radius, step, angle) {
   for (let dist = 0; dist < radius; dist += step) {
     //calculate the angle
     let theta = asin(dist / radius);
-    //calculate x,y based on angle
+    //calculate x,y based on the angle
     let x1 = radius * cos(theta);
     let y1 = radius * sin(theta);
     //get the opposite side of the circle
@@ -17,7 +17,9 @@ hatchedCircle = function (x, y, radius, step, angle) {
     let y3 = -1 * y1;
     let x4 = x2;
     let y4 = -1 * y2;
+    //draw the lines
     line(x1, y1, x2, y2);
+    //avoid drawing the center line twice in case alpha is used in the stroke color
     if (dist > 0) {
       line(x3, y3, x4, y4);
     }
